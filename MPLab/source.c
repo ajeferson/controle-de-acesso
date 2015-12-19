9cf66ad9850391b9ca82e62aae29328ac7553d8d
Alan Jeferson <alan.jeferson11@gmail.com>
2015-12-18 21:07:50 -0300

    Making the keyboard sweeping

--- MPLab/source.c
+++ MPLab/source.c
@@ -8,15 +8,26 @@
 #pragma config PBADEN = OFF   /// PORTB.RB0,1,2,3,4 AS I/O DIGITAL
 #pragma config MCLRE = OFF
 
-// LCD variables
+// Variaveis do LCD
 #define LCD_RS LATEbits.LATE0
 #define LCD_RW LATEbits.LATE2
 #define LCD_E LATEbits.LATE1
 #define LCD_DATA LATD
 
+// Variaveis do teclado
+#define Linha_1 LATBbits.LATB4
+#define Linha_2 LATBbits.LATB5
+#define Linha_3 LATBbits.LATB6
+#define Linha_4 LATBbits.LATB7
+#define Coluna_1 PORTBbits.RB3
+#define Coluna_2 PORTBbits.RB2
+#define Coluna_3 PORTBbits.RB1
+#define Coluna_4 PORTBbits.RB0
+
 // Include da galera
 #include "lcd.h"
 #include "menu.h"
+#include "teclado.h" 
 
 #pragma mark Definicao das funcoes
 void configuraSistema();
@@ -24,16 +35,17 @@ void lcdEnviaInstrucao(char instrucao);
 void lcdEnviaDado(char dado);
 void delay_us(int quantidade);
 
-#pragma mark Implementacao da funcoes
-
 /** Seta os tris da galera */
 void configuraSistema() {
 	ADCON1=0x0F;
 	TRISD=0b00000000;  
 	TRISE=0b11111000;
+	TRISB=0b00001111;
 }
 
 void main() {
+
+	char tecla;
 
 	configuraSistema();
 
@@ -47,5 +59,6 @@ void main() {
 	appendaSenha('*', 3);
 
 	while(1) {
+		tecla = varreTeclado();
 	}
 }
---
+++ MPLab/teclado.h
@@ -0,0 +1,53 @@
+#ifndef TECLADO_H_INCLUDED
+#define TECLADO_H_INCLUDED
+
+// Definicoes
+
+/** Faz a varredura do teclado e retorna a tecla apertada. */
+char varreTeclado();
+
+
+// Implementacoes
+
+char varreTeclado() {
+	Linha_1=1; //varrendo a primeira linha
+	Linha_2=0;
+	Linha_3=0;
+	Linha_4=0;
+
+	if (Coluna_1) { while (Coluna_1) {} return 1;}
+	if (Coluna_2) { while (Coluna_2) {} return 2;}
+	if (Coluna_3) { while (Coluna_3) {} return 3;}
+	if (Coluna_4) { while (Coluna_4) {} return 'A';}
+
+	Linha_1=0; //varrendo a segunda linha
+	Linha_2=1;
+	Linha_3=0;
+	Linha_4=0;
+	if (Coluna_1) { while (Coluna_1) {} return 4;}
+	if (Coluna_2) { while (Coluna_2) {} return 5;}
+	if (Coluna_3) { while (Coluna_3) {} return 6;}
+	if (Coluna_4) { while (Coluna_4) {} return 'B';}
+	Linha_1=0; //varrendo a terceira linha
+	Linha_2=0;	
+	Linha_3=1;
+	Linha_4=0;
+	if (Coluna_1) { while (Coluna_1) {} return 7;}
+	if (Coluna_2) { while (Coluna_2) {} return 8;}
+	if (Coluna_3) { while (Coluna_3) {} return 9;}
+	if (Coluna_4) { while (Coluna_4) {} return 'C';}
+
+	Linha_1=0; //varrendo a quarta linha
+	Linha_2=0;
+	Linha_3=0;
+	Linha_4=0;
+	if (Coluna_1) { while (Coluna_1) {} return '*';}
+	if (Coluna_2) { while (Coluna_2) {} return 0;}
+	if (Coluna_3) { while (Coluna_3) {} return '#';}
+	if (Coluna_4) { while (Coluna_4) {} return 'D';}
+
+	return '0';
+
+}
+
+#endif
